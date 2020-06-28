5982be282aa979b366da62a772b0a627534e3765
Kinnar Kansara <kinnarrk@gmail.com>
2020-06-28 16:22:54 -0400

    deployment from circleci and appspec

---
+++ codedeploy/ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+# update instance
+# sudo apt -y update
+
+# install general libraries like Java or ImageMagick
+# apt-get -y install default-jre ImageMagick
+
+# install pm2 module globaly
+npm list pm2 || npm install -g pm2
+pm2 update
+
+if [ -d "~/node" ] 
+then
+    cd ~/node
+    pm2 stop www || true
+    cd ~
+    rm -rf node
+fi
---
+++ codedeploy/post_install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+cd ~/node
+npm install
+
+# setup NODE_ENV
+if [ ! -z "$DEPLOYMENT_GROUP_NAME" ]; then
+    export NODE_ENV=$DEPLOYMENT_GROUP_NAME
+
+    hasEnv=`grep "export NODE_ENV" ~/.profile | cat`
+    if [ -z "$hasEnv" ]; then
+        echo "export NODE_ENV=$DEPLOYMENT_GROUP_NAME" >> ~/.profile
+    else
+        sed -i "/export NODE_ENV=\b/c\export NODE_ENV=$DEPLOYMENT_GROUP_NAME" ~/.profile
+    fi
+fi
+
+# add node to startup
+hasRc=`grep "su -l $USER" /etc/rc.local | cat`
+if [ -z "$hasRc" ]; then
+    sudo sh -c "echo 'su -l $USER -c \"cd ~/node;sh ./run.sh\"' >> /etc/rc.local"
+fi
